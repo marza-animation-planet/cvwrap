0377768e7f06610a8fdab82bc346c135cfb0158a
Junsei Ko <ko2@marza.com>
2023-05-25 13:41:06 +0900

    update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -2,10 +2,6 @@
 
 #./docker-builder/build_project.sh $PATH_TO_SOURCE_CODE "↓↓↓ build command ↓↓↓" 
 
-#echo "===Building for Maya 2020..."
-#rm -rf ./excons.cache
-#scons with-maya=2020 devtoolset=11
-
 echo "===Building for Maya 2022..."
 rm -rf ./excons.cache
 scons37 with-maya=2022 devtoolset=11
@@ -16,4 +12,4 @@ scons39 with-maya=2023 devtoolset=11
 
 echo "===Building for Maya 2024..."
 rm -rf ./excons.cache
-scons with-maya=2024 devtoolset=11
+scons310 with-maya=2024 devtoolset=11
